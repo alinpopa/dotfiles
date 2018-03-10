d1c12d0e514cd5ca48ea79928a5acdd22c221e1f
Alin Popa <alin.popa@gmail.com>
2018-03-10 14:02:23 +0000

    Add some http static serving go utility

---
+++ bin/static.go
@@ -0,0 +1,29 @@
+/*
+Build by using `go build static.go`
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func loggingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	})
+}
+
+func main() {
+	port := flag.Int("p", 8888, "port to serve on")
+	flag.Parse()
+	log.Printf("Serving on HTTP port %d\n", *port)
+	http.Handle("/", loggingHandler(http.FileServer(http.Dir("."))))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
